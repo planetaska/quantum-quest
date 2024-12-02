61542f67021ca2d26f913a204c74e7bd846cba33
planetaska <planetaska@gmail.com>
2024-12-02 14:05:33 -0800

    Finish main game step

--- dirt.gd
+++ dirt.gd
@@ -4,6 +4,17 @@ const SPEED = 300.0
 
 var adding = false
 
+func saveConfig(x, y):
+	var save_file = FileAccess.open("./algorithms/config.json", FileAccess.WRITE)
+	var config = {
+		"board_size": 8,
+		"start_pos": [0, 0],
+		"road_blocks": [[1,1],[2,2],[3, 3], [4, 4]],
+		"goal_pos": [[x, y]]
+	}
+	var json_string = JSON.stringify(config)
+	save_file.store_line(json_string)
+
 func _unhandled_input(event):
 	if event.is_action_pressed("LeftClick"):
 		adding = true
@@ -16,4 +27,11 @@ func _physics_process(delta):
 	if adding:
 		#print(get_tree().root.get_node("main-screen/tile_map/TileMap").selectedTile)
 		#print($"../tile_map/TileMap".selectedTile)
-		global_position = Vector2($"../tile_map/TileMap".selectedTile) + Vector2(60,60)
+		var selectedTile = $"../tile_map/TileMap".selectedTile
+		#print(selectedTile)
+		global_position = Vector2(selectedTile) + Vector2(160,100)
+		
+		var x = (int(selectedTile[0]) -28) / 56
+		var y = (int(selectedTile[1]) -28) / 56
+		saveConfig(x,y)
+		adding = false
--- scripts/BtnStart.gd
+++ scripts/BtnStart.gd
@@ -9,8 +9,26 @@ func _ready():
 func _on_pressed():
 	var node = $"../robot"
 	
-	var route = [[0, 0], [0, 2], [2, 1], [5, 5], [2, 1], [2, 5], [6, 6], [5, 1], [4, 5], [0, 7], [3, 7], [4, 6], [1, 7], [2, 0], [2, 7], [3, 2], [2, 4], [4, 7], [0, 4], [2, 3], [0, 6], [1, 3], [3, 7], [3, 4], [7, 0], [1, 3], [6, 3], [2, 1], [0, 1], [2, 3], [5, 1], [5, 7], [2, 1], [6, 7], [3, 2], [7, 4], [2, 5], [0, 1], [6, 1], [6, 5], [4, 7], [2, 2], [3, 5], [7, 2], [5, 0], [7, 5], [0, 5], [7, 4], [3, 7], [7, 7]]
+	var py_path = "/Users/aska/lab/python/qiskit/bin/python3"
+	var alg_path = ["./algorithms/dijkstra.py", "./algorithms/config.json"]
+	var output = []
 	
-	for pos in route:
+	OS.execute(py_path, alg_path, output)
+	
+	print(output)
+	
+	#return
+	
+	var file = "./route.json"
+	
+	var json_as_text = FileAccess.get_file_as_string(file)
+	var route = JSON.parse_string(json_as_text)
+	
+	if route:
+		print(route.path)
+	
+	#var route = [[0, 0], [0, 2], [2, 1], [5, 5], [2, 1], [2, 5], [6, 6], [5, 1], [4, 5], [0, 7], [3, 7], [4, 6], [1, 7], [2, 0], [2, 7], [3, 2], [2, 4], [4, 7], [0, 4], [2, 3], [0, 6], [1, 3], [3, 7], [3, 4], [7, 0], [1, 3], [6, 3], [2, 1], [0, 1], [2, 3], [5, 1], [5, 7], [2, 1], [6, 7], [3, 2], [7, 4], [2, 5], [0, 1], [6, 1], [6, 5], [4, 7], [2, 2], [3, 5], [7, 2], [5, 0], [7, 5], [0, 5], [7, 4], [3, 7], [7, 7]]
+	
+	for pos in route.path:
 		await get_tree().create_timer(0.3).timeout
-		node.position = Vector2( (pos[0]*64)+90, (pos[1]*64)+90 )
+		node.position = Vector2( (pos[0]*56)+190, (pos[1]*56)+130 )
